2919f705f7ad63039ddb49e85ec211c69d6ff311
JnilKerb <1091525766@qq.com>
2022-06-19 21:06:16 +0800

    hot-fix-first-commit2

--- src/main/java/com/oa_item/git/test.java
+++ src/main/java/com/oa_item/git/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("这是第一次修改！！！");
         System.out.println("这是hot-fix 第一次修改！！！");
+        System.out.println("这是hot-fix 第二次修改！！！");
     }
 }
